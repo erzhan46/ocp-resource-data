6f0faf3e5fe5fbdee493edfa1ce6b869fa87dedf
Yerzhan Beisembayev <ybeisemb@redhat.com>
2021-04-19 10:46:01 -0500

    Initial commit

---
+++ gather-resource-data.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Script to gather resource data for specified projects
+# v0.1 Yerzhan Beisembayev ybeisemb@redhat.com
+
+
+
+#oc describe node | grep dev-sap | sed 's/\([0-9]*\)m (/0.\1 (/' | sed 's/\([0-9]*\)Mi (/\1 (/' | sed 's/\([0-9]*\)Gi (/\1 (/'
